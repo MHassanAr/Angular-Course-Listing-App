f9bf0ae4c4630086e9a49a503b62ba9ab22b01be
MHassanAr <mhassana2344@gmail.com>
2024-12-18 11:44:11 +0500

    Add routes to the routes.ts and main.ts

--- angular-course-listing-app/src/main.ts
+++ angular-course-listing-app/src/main.ts
@@ -1,6 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideRouter, Routes } from '@angular/router';
 import { AppComponent } from './app/app.component';
+import { HomeComponent } from './app/pages/home/home.component';
+import { AboutComponent } from './app/pages/about/about.component';
+import { CoursesComponent } from './app/pages/courses/courses.component';
+import { ContactComponent } from './app/pages/contact/contact.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'courses', component: CoursesComponent },
+  { path: 'contact', component: ContactComponent },
+];
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)],
+});
