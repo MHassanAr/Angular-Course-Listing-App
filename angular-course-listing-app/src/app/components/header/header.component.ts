8340585b2c13348e385a4e8c5c3775a243d15967
MHassanAr <mhassana2344@gmail.com>
2024-12-18 11:38:44 +0500

    Updated the header links and add router link for the navigation from one page to another

--- angular-course-listing-app/src/app/components/header/header.component.ts
+++ angular-course-listing-app/src/app/components/header/header.component.ts
@@ -4,10 +4,8 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
