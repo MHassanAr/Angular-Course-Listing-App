3e6894bb878f0838c27eb8987802c050074d5471
MHassanAr <mhassana2344@gmail.com>
2024-12-22 00:04:18 +0500

    Create logic to handle login status

--- angular-course-listing-app/src/app/app.component.ts
+++ angular-course-listing-app/src/app/app.component.ts
@@ -1,16 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./components/header/header.component";
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { RouterOutlet } from '@angular/router'; // Import RouterOutlet
 import { FooterComponent } from './components/footer/footer.component';
-
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [FooterComponent, HeaderComponent, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular-course-listing-app';
+export class AppComponent implements OnInit {
+  title = 'web-proj';
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    // clear login status if navigating to public pages
+    this.router.events.subscribe(() => {
+      if (
+        this.router.url === '/' ||
+        this.router.url === '/about' ||
+        this.router.url === '/courses' ||
+        this.router.url === '/contact'
+      ) {
+        localStorage.setItem('isLoggedIn', 'false'); // Clear login status
+      }
+    });
+  }
 }
