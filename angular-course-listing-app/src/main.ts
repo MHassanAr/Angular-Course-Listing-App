4b727d2daa32e961f450c4c02fe049623ef597db
MHassanAr <mhassana2344@gmail.com>
2024-12-21 22:46:12 +0500

    Import newly created compoennts in main.ts

--- angular-course-listing-app/src/main.ts
+++ angular-course-listing-app/src/main.ts
@@ -5,12 +5,17 @@ import { HomeComponent } from './app/pages/home/home.component';
 import { AboutComponent } from './app/pages/about/about.component';
 import { CoursesComponent } from './app/pages/courses/courses.component';
 import { ContactComponent } from './app/pages/contact/contact.component';
+import { LoginComponent } from './app/pages/login/login.component';
+import { AdminComponent } from './app/pages/admin/admin.component';
+import { authGuard } from './app/guards/auth.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'courses', component: CoursesComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'admin', component: AdminComponent, canActivate: [authGuard] },
 ];
 
 bootstrapApplication(AppComponent, {
