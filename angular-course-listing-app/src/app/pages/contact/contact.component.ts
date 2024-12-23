5d3b51e7bfecb9c5d59cbd86b59fee9e0e06a2ad
GHAURI4434 <dilshadghauri1@gmail.com>
2024-12-23 23:50:16 +0500

    write logic for contact page

--- angular-course-listing-app/src/app/pages/contact/contact.component.ts
+++ angular-course-listing-app/src/app/pages/contact/contact.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrls: ['./contact.component.css'],
+  imports: [FormsModule, CommonModule],
 })
 export class ContactComponent {
+  contact = {
+    name: '',
+    email: '',
+    message: '',
+  };
 
+  successMessage: string | null = null;
+
+  onSubmit(contactForm: NgForm) {
+    if (contactForm.invalid) {
+      return;
+    }
+
+    // Display success message
+    this.successMessage = 'Form submitted successfully!';
+
+    // Reset the form values and validation state
+    contactForm.resetForm();
+
+    // Clear the success message after 5 seconds
+    setTimeout(() => {
+      this.successMessage = null;
+    }, 5000);
+  }
 }
